254170a37b253aace56fbcbc8069772ba0b0270b
Edjere Evelyn <evelynedjere@gmail.com>
2025-03-09 02:19:38 +0100

    Remove unused whitespace and add ProjectionIcon component

--- src/app/transcription/page.jsx
+++ src/app/transcription/page.jsx
@@ -40,9 +40,6 @@ const LoadingSpinner = () => (
   </svg>
 );
 
-
-
-
 // New ProjectionIcon component
 const ProjectionIcon = () => (
   <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
